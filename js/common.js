69e4279e672b9b0f9588c5d69695ed28880e978c
sk <wlalstm1@gmail.com>
2021-10-08 18:22:34 +0900

    비대면 사실조사 시스템 퍼블리싱 완료

--- js/common.js
+++ js/common.js
@@ -1,11 +1,5 @@
 ﻿$(document).ready(function(){
-    /* //파일찾기
-    $('.input_file').change(function(){
-        var i = $(this).val();
-        $('.upload_text').val(i);
-    });*/
-
-    /* lnb_menu 클릭 시 */
+    // lnb_menu
     $('.lnb_menu-list .btn_depth').click(function(){
         if ($(this).parent('.list_item').hasClass('is-active')) {
             slideUp();
@@ -19,9 +13,37 @@
         };
     })
 
-    // toggle 공통
-    $('.btn_toggle').click(function() {
-        $(this).toggleClass('is-active');
+    //datepicker
+    $("input.datepicker").datepicker({
+        inline: true,
+        showOn: "button",
+        dateFormat: "yy.mm.DD",
+        buttonText: "Select date",
+        changeYear: true,
+        changeMonth: true,
+        showOtherMonths: true,
+        monthNamesShort: [ "1","2","3","4","5","6","7","8","9","10","11","12" ],
+        dayNames: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+        dayNamesShort: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+
+        beforeShow: function(input, inst)
+        {
+            var calendar = inst.dpDiv;
+            setTimeout(function() {
+                calendar.position({
+                    my: 'right top',
+                    at: 'right bottom',
+                    collision: 'none',
+                    of: input
+                });
+            }, 1);
+        },
+        onSelect: function() {
+            $('.ui-widget-overlay.ui-front').remove();
+        },
+        onClose: function() {
+            $('.ui-widget-overlay.ui-front').remove();
+        }
     });
 
 });
